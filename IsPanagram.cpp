411ecd15fa6d4a79898b72417bd56c0727d62064
GSriyarsha <48006019+GSriyarsha@users.noreply.github.com>
2019-06-18 11:22:17 +0530

    Create IsPanagram.cpp

---
+++ IsPanagram.cpp
@@ -0,0 +1,43 @@
+#include <string>
+#include <iostream>
+#include <ctype.h>
+
+public class IsPanagram {
+    void sortString (String input){
+		int len = input.length();
+		  for ( int i = 0; i <  len; i ++){
+                for  ( int j =  0;  j < len; j ++) {
+                        if (strcmp(input [ j - 1 ],  input [ j ]) > 0){
+                                strcpy (sortInputString, input [ j - 1 ]);
+                                strcpy (input [ j - 1 ], input [ j ] );
+                                strcpy ( input [ j ], sortInputString);
+                        }
+                }
+        }
+		panagram(input, len);
+	}
+	boolean panagram(String Array, int len){
+		int count = 0;
+		if (len == 26){
+			for ( int  i = 0; i < len; i ++){
+				if ((i != len - 1) && (((int)input[i]) > ((int)input[i - 1]))){
+					count ++;
+				}
+				else {
+					break;
+				}
+			}
+		}
+		if (count == 26){
+			return true;
+		}
+		return false;
+	}
+
+	int main() {
+		String input = argsv[0];
+    tolower(input);
+		sortString (input);
+	}
+
+}
