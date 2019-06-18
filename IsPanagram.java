92a597aae0f715b7b5eda36bde9359e8d1ecc573
GSriyarsha <48006019+GSriyarsha@users.noreply.github.com>
2019-06-18 11:18:15 +0530

    Create IsPanagram.java

---
+++ IsPanagram.java
@@ -0,0 +1,35 @@
+import java.io.*;
+import java.util.Arrays;
+
+public class IsPanagram {
+	public static void sortString (String input){
+		int len = input.length();
+		char[] inputArray = input.toCharArray();
+		Arrays.sort(inputArray);
+		 panagram(inputArray, len);
+	}
+	public static boolean panagram(char[] inputArray, int len){
+		int count = 0;
+		if (len == 26){
+			for ( int  i = 0; i < len; i ++){
+				if ((i != len - 1) && (((int)inputArray[i]) > ((int)inputArray[i - 1]))){
+					count ++;
+				}
+				else {
+					break;
+				}
+			}
+		}
+		if (count == 26){
+			return true;
+		}
+		return false;
+	}
+
+	public static void main(String[] args) {
+		String input = args[0];
+    input.toLowerCase();
+  	sortString (input);
+	}
+
+}
